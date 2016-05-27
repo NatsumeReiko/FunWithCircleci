63d840201619a4926f8b7b8bb27c6415ff6de87f
NatsumeReiko <sprix.reiko.natsume@gmail.com>
2016-05-27 16:25:45 +0900

    add something

--- app/src/test/java/amy/com/funwithcircleci/UnitTestTargetTest.java
+++ app/src/test/java/amy/com/funwithcircleci/UnitTestTargetTest.java
@@ -12,10 +12,10 @@ public class UnitTestTargetTest {
     public void return10_001() throws Exception {
         assertEquals("10", UnitTestTarget.return10(""));
     }
-    @Test
-    public void return10_002_fail() throws Exception {
-        assertEquals("5", UnitTestTarget.return10(""));
-    }
+//    @Test
+//    public void return10_002_fail() throws Exception {
+//        assertEquals("5", UnitTestTarget.return10(""));
+//    }
 
     @Test
     public void return10_003() throws Exception {
