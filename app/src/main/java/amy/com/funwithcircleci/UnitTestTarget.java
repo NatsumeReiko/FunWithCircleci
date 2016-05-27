96ae090bf7880d6d9a053d6b34aa7da8e165db40
NatsumeReiko <sprix.reiko.natsume@gmail.com>
2016-05-27 16:00:30 +0900

    add something

---
+++ app/src/main/java/amy/com/funwithcircleci/UnitTestTarget.java
@@ -0,0 +1,8 @@
+package amy.com.funwithcircleci;
+
+public class UnitTestTarget {
+    public static String return10(String srcString) {
+        return "10";
+    }
+
+}
---
+++ app/src/test/java/amy/com/funwithcircleci/UnitTestTargetTest.java
@@ -0,0 +1,19 @@
+package amy.com.funwithcircleci;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class UnitTestTargetTest {
+    @Test
+    public void return10_001() throws Exception {
+        assertEquals("10", UnitTestTarget.return10(""));
+    }
+    @Test
+    public void return10_002() throws Exception {
+        assertEquals("5", UnitTestTarget.return10(""));
+    }
+}
